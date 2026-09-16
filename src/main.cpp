bb34ee7fa1d91cb3107c11dec9f85d7f213cc780
aliKumailq <aqazilba@ualberta.ca>
2026-09-15 22:43:23 -0600

    Done

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <cstdlib>
 #include "SharedPtr.h"
 #include <iostream>
-using poop = std::pair<int,int>;
+using test_type = std::pair<int,int>;
 
 int main() {
    SharedPtr<int> t = makeSharedBasic<int>(99);
    std::cout << *t << " refCount: " << t.useCount() << "\n";
-   // 99 1
+   // 99 refCount: 1
 
    
 
@@ -14,14 +14,18 @@ int main() {
    a = t;
    *a = 32;
    std::cout << *a << " refCount: " << a.useCount() << "\n";
-   // 32 2
+   // 32 refCount: 2
 
    t = makeShared<int>(42);
-   std::cout << *t << " refCount: " << t.useCount() << "\n"; // 42 1
-   std::cout << *a << " refCount: " << a.useCount() << "\n"; // 32 1
+   std::cout << *t << " refCount: " << t.useCount() << "\n"; 
 
-   SharedPtr<poop> myPair = makeSharedBasic<poop>(1,2);  
+   // 42 refCount: 0
+   // 32 refCount: 1
+
+   SharedPtr<test_type> myPair = makeSharedBasic<test_type>(1,2);  
    printf("%d %d\n", myPair->first, myPair->second);
+
+   // 1 2
 
 
 
